b80808c64ee5024901a938bdee1a200fb6b24146
VitaliyStrashnenko <smitwerty@gmail.com>
2020-03-26 08:46:37 +0000

    terraform-1 try 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 terraform {
   # Версия terraform
-  required_version = "0.12.20"
+  #required_version = "0.12.20"
 }
 
 provider "google" {
   # Версия провайдера
-  version = "2.15.0"
+  #version = "2.15.0"
 
   # ID проекта
   project = var.project
