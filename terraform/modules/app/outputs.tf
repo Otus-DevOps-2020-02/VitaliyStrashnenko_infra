8047334ee80c1d38746e7d7cc790df05084f8d19
VitaliyStrashnenko <smitwerty@gmail.com>
2020-04-11 15:44:43 +0000

    HW ansible-1 try1

--- terraform/modules/app/outputs.tf
+++ terraform/modules/app/outputs.tf
@@ -1,3 +1,10 @@
 output "app_external_ip" {
-  value = google_compute_instance.app.network_interface.0.access_config.0.assigned_nat_ip
+ # value = google_compute_instance.app.network_interface[0]access_config[0]assigned_nat_ip
+
+ value = "${google_compute_instance.app.network_interface.0.access_config.0.nat_ip}"
+
+}
+
+output "app_static_ip" {
+  value = google_compute_address.app_ip.address
 }
